69c9d672501664c072dc392f931d7e6c32cbb709
SonPamungkas <nononon928@gmail.com>
2026-08-10 11:25:08 +0700

    Update plugin ID and version for KISS Mod

--- Plugin.cs
+++ Plugin.cs
@@ -2,42 +2,31 @@ using System;
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
-
 namespace KISSMod
 {
-    [BepInPlugin("com.kiss", "KISS Mod (Kinetic Interception Secondary System)", "1.0.0")]
+    [BepInPlugin("neutral.kiss", "KISS Mod (Kinetic Interception Secondary System)", "1.1.0")]
     public class KISSPlugin : BaseUnityPlugin
     {
         public static KISSPlugin Instance;
         private Harmony _harmony;
-
         private void Awake()
         {
             Instance = this;
-            _harmony = new Harmony("com.kiss");
-            
-            // Patch standard Awake
+            _harmony = new Harmony("neutral.kiss");
             _harmony.Patch(
                 AccessTools.Method(typeof(Missile), "Awake"),
                 postfix: new HarmonyMethod(AccessTools.Method(typeof(Missile_Awake_Patch), "Postfix"))
             );
-            
             var prefixMethod = new HarmonyMethod(AccessTools.Method(typeof(Warhead_Detonate_Patch), "Prefix"));
-
-            // Patch vanilla Warhead.Detonate
-            var originalDetonate = AccessTools.Method(AccessTools.Inner(typeof(Missile), "Warhead"), "Detonate");
+            var originalDetonate = AccessTools.Method(typeof(Missile.Warhead), "Detonate");
             if (originalDetonate != null)
             {
                 _harmony.Patch(originalDetonate, prefix: prefixMethod);
             }
-
             Log("KISS Mod loaded, Muach!");
         }
-
         private void Start()
         {
-            // Compatibility: Patch NOKillWeapons' Detonate override if it exists
-            // We do this in Start() to avoid load order dependency issues with other mods (like QoL).
             var nokwDetonate = AccessTools.Method("NOKW.Patches.KillsLogging.MissileExtensions:Detonate");
             if (nokwDetonate != null)
             {
@@ -46,13 +35,11 @@ namespace KISSMod
                 Log("KISS Mod: NOKillWeapons compatibility enabled.");
             }
         }
-
         public void Log(string msg)
         {
             Logger.LogInfo(msg);
         }
     }
-
     public static class Warhead_Detonate_Patch
     {
         public static void Prefix(ref bool armed)
@@ -60,17 +47,13 @@ namespace KISSMod
             armed = true;
         }
     }
-
     public static class Missile_Awake_Patch
     {
         private static readonly AccessTools.FieldRef<Missile, float> GLimitRef =
             AccessTools.FieldRefAccess<Missile, float>("gLimit");
-
         public static void Postfix(Missile __instance)
         {
             if (__instance == null) return;
-            // Disable G-limit so munitions don't die prematurely from physics spikes
-            // when hitting the ground at supersonic speeds.
             GLimitRef(__instance) = float.MaxValue;
         }
     }
